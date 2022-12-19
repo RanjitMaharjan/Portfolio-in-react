6f47c8bd44ae0669e2551e95446efc6cffce7e4a
RanjitMaharjan <gwenmhrzn25@gmail.com>
2022-12-19 15:06:10 +0545

    about exp & service added

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,96 @@
 import React from 'react'
 import './experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="experience">
+      <h5>What i Know</h5>
+      <h2>I Know</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>JAVASCRIPT</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>BOOTSTRAP</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>REACT</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience_backend">
+        <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>NODE JS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>MONGO DB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>MYSQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill className='experience_details-icons' />
+              <div>
+                <h4>PYTHON</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,11 +1,92 @@
 import React from 'react'
 import './service.css'
+import { BiCheck } from 'react-icons/bi'
 
 const Service = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="service">
+      <h5>What I Offer</h5>
+      <h2>service</h2>
+
+      <div className="container service_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>UI/UX Desgin</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of ui ux */}
+        <article className="service">
+          <div className="service_head">
+            <h3>WEB DEVELOPMENT</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of web developmenmt */}
+        <article className="service">
+          <div className="service_head">
+            <h3>CONTENT CREATION</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+            <li>
+              <BiCheck className='service_list-icons' />
+              <p>Welcome To Devil's Hunter Club .</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of content */}
+      </div>
+    </section>
   )
 }
 
